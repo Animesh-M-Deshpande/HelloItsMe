be29eef58243cf7c57c83875daba4b6c09a53446
amdesh01 <animeshdeshpande@gmail.com>
2017-07-24 23:13:35 +0530

    Initial Commit

---
+++ app/src/main/java/com/kimdho/helloitsme/HeyItsMe.java
@@ -0,0 +1,13 @@
+package com.kimdho.helloitsme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeyItsMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hey_its_me);
+    }
+}
